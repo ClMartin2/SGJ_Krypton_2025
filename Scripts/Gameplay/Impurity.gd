ec24bf32b9d8c9b4f39057adf9f5e1cd85025a00
ClMartin2 <clmartinpro1@gmail.com>
2025-03-30 12:21:26 +0200

    [ADD] All level [UPDATE] Impurity visual

--- Scripts/Gameplay/Impurity.gd
+++ Scripts/Gameplay/Impurity.gd
@@ -6,6 +6,6 @@ func _process(delta: float) -> void:
 	rotation += rotationSpeed * delta
 	
 func _on_body_entered(body: Node2D) -> void: 
-	if(body is Player):
+	if(body is Player and !Global.superconduct):
 		GameManager.AddUI(looseScreen)
 		GameManager.PauseGame()
