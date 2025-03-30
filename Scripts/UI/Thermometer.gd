33a80c54d3f1929cafc0361b434e4a3a23ed2554
ClMartin2 <clmartinpro1@gmail.com>
2025-03-30 14:37:59 +0200

    [ADD] Thermometer

---
+++ Scripts/UI/Thermometer.gd
@@ -0,0 +1,18 @@
+extends TextureProgressBar
+
+var maxTemperature := -100.0
+var minTemperature := -210.0
+@export var colorMaxTemperature := Color(1, 0, 0)
+@export var colorMinTemperature := Color(0, 0, 1)
+
+func _process(delta: float) -> void:
+	var pourcentageValue = 1 - ((Global.temperature - maxTemperature) / (minTemperature - maxTemperature))
+
+	# Assurer que la valeur reste entre 0 et 1
+	pourcentageValue = clamp(pourcentageValue, 0.0, 1.0)
+
+	# Appliquer la valeur à la barre de progression
+	value = pourcentageValue * 100
+
+	# Interpolation de la couleur
+	tint_progress = colorMinTemperature.lerp(colorMaxTemperature, pourcentageValue)
